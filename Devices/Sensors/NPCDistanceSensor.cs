d3ddffd21c1d8f48997a1f5e655b65b3ee3c5219
Mathieu Lomax <mat.lomax@gmail.com>
2019-07-25 13:39:27 +0100

    Add filters to NPC distance sensor

--- Devices/Sensors/NPCDistanceSensor.cs
+++ Devices/Sensors/NPCDistanceSensor.cs
@@ -10,12 +10,15 @@ namespace WireMod.Devices
 		public NPCDistanceSensor()
 		{
 			this.Name = "Nearest NPC Distance Sensor";
-			this.Width = 1;
-			this.Height = 2;
+			this.Width = 2;
+			this.Height = 3;
+			this.Origin = new Point16(1, 1);
 
 			this.PinLayout = new List<PinDesign>
 			{
-				new PinDesign("Out", 0, new Point16(0, 1), "int"),
+				new PinDesign("In", 0, new Point16(0, 1), "bool", "Hostile Filter"),
+				new PinDesign("In", 1, new Point16(1, 0), "bool", "Character Filter"),
+				new PinDesign("Out", 0, new Point16(1, 2), "int"),
 			};
 		}
 
@@ -23,9 +26,19 @@ namespace WireMod.Devices
 
 		private int GetOutput()
 		{
-			var npc = Main.npc.Select(n => (this.LocationRect.Location.ToWorldCoordinates() - n.position).Length()).OrderBy(n => n).FirstOrDefault();
+			var npc = Main.npc.Select(n => n);
 
-			return (int)npc;
+			if (this.Pins["In"][0].IsConnected() && int.TryParse(this.Pins["In"][0].GetValue(), out var hostile))
+			{
+				npc = npc.Where(n => n.friendly == (hostile == 0));
+			}
+
+			if (this.Pins["In"][1].IsConnected() && int.TryParse(this.Pins["In"][1].GetValue(), out var character))
+			{
+				npc = npc.Where(n => n.townNPC == (character == 1));
+			}
+
+			return (int)npc.Select(n => (this.LocationRect.Location.ToWorldCoordinates() - n.position).Length()).OrderBy(n => n).FirstOrDefault();
 		}
 	}
 }
